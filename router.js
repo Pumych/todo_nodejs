62dc02fd1793bba157d6d65f4688f783670a9f3f
pumych <pumych@gmail.com>
2014-08-18 01:33:16 +0300

    Router updated

--- router.js
+++ router.js
@@ -1,57 +1,21 @@
 var url = require('url');   // URL resolution and parsing module
 var fs = require('fs');     // File System module
 
-/**
- * Sends response (header and file) to request by file type
- *
- * @param pathname
- * @param fileType - 'css', 'js' etc.
- * @param response
- */
-var responseByType = function( pathname, fileType, response ){
-    var contentType;
-    var status = 200;
-    var encoding = 'utf8';
-    switch( fileType ){
-        case 'css': contentType = 'text/css';
-            break;
-        case 'js': contentType = 'text/javascript';
-            break;
-        case 'jpg':
-            contentType = 'image/jpeg';
-            encoding = 'binary';
-            break;
-        default: contentType = 'unknown/unknown';
-    }
-
-    fs.readFile(__dirname + pathname, encoding, function(err, data){
-        if(err) throw err;
-        response.writeHead(status, {'Content-Type': contentType});
-        response.write(data, encoding);
-        response.end();
-    });
-
-
-}
-
 exports.get = function(request, response){
     var requestUrl = url.parse(request.url, true);  // Parse requested URL
     var pathname = requestUrl.pathname;             // Get pathname
-
-//    console.log( 'requestUrl: ', requestUrl );
-//    console.log( 'pathname:', pathname );
 
     /**
      * Route by requested filename extension
      */
     if( /.(css)$/.test( pathname ) ){
-        responseByType(pathname, 'css', response);
+        responseDataByRequestType(pathname, 'css', response);
 
     } else if(/.(js)$/.test(pathname)){
-        responseByType(pathname, 'js', response);
+        responseDataByRequestType(pathname, 'js', response);
 
     } else if(/.(jpg)$/.test(pathname)){
-        responseByType(pathname, 'jpg', response);
+        responseDataByRequestType(pathname, 'jpg', response);
 
     } else {
         var controllerPath;
@@ -66,4 +30,71 @@ exports.get = function(request, response){
 
         require('./controllers/' + controllerPath).get(request, response);
     }
+};
+
+/**
+ * Sends response (header and file) to request by file type
+ *
+ * @param pathname
+ * @param fileExt - 'css', 'js', 'jpg' etc.
+ * @param response
+ */
+
+var responseDataByRequestType = function( pathname, fileExt, response ){
+    var router = new Router();
+    router.setFileExt( fileExt );
+
+    /** There is state of wrong css/js... file */
+//    var data;
+//    try {
+//        data = fs.readFile(__dirname + pathname, router.getEncoding());
+//    } catch( e ) {
+//        response.writeHead('404', {'Content-Type': 'text/plain'});
+//        response.write('>>> 404 err');
+//    } finally {
+//        response.writeHead(router.getStatus(), {'Content-Type': router.getContentType()});
+//        response.write(data, router.getEncoding());
+//        response.end();
+//    }
+
+    fs.readFile(__dirname + pathname, router.getEncoding(), function(err, data){
+        if(err) throw err;
+        response.writeHead(router.getStatus(), {'Content-Type': router.getContentType()});
+        response.write(data, router.getEncoding());
+        response.end();
+    });
+
+
 }
+
+var Router = function(){
+    var _ = this;
+    var fileExt = '';
+
+    this.setFileExt = function( fileExt ){
+        _.fileExt = fileExt;
+    }
+
+    this.getContentType = function(){
+        switch( _.fileExt ){
+            case 'css': return 'text/css';
+            case 'js': return 'text/javascript';
+            // TODO: Add all file types
+            case 'jpg': return 'image/jpeg';
+            default: return 'unknown/unknown';
+        }
+    }
+
+    this.getStatus = function(){
+        return 200;
+    }
+
+    this.getEncoding = function(){
+        switch( _.fileExt ){
+            case 'jpg':
+            case 'png':
+            case 'img': return 'binary';
+            default: return 'utf8';
+        }
+    }
+};
